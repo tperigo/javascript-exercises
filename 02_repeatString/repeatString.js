c8584daa1b29d78c79a4f7b55b726ad1ff86a72b
Theo Perigo <tperigo@akatsuki.lan>
2023-08-09 13:22:34 -0600

    completed exercises 01,02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (str, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
+    let output = "";
+    for (let i = 0; i <= num - 1; i++) {
+        output = output + str;
+    }
+    return output;
 };
 
 // Do not edit below this line
